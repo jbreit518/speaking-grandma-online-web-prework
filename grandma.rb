93b68b366ef365f7aba629afa96714a1cf90da1f
Justine Berretta <justberretta@gmail.com>
2019-05-16 21:54:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma()
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma == .upcase
+  elsif speak_to_grandma == speak_to_grandma.upcase
     puts "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
